a5471ebd4e9032e216bee0dca21701383bdb39a8
Marc-Antoine Bourgeot <marc-antoine.bourgeot@spotme.com>
2024-10-02 21:14:22 +0200

    Solve arrays minimum_swaps_2

--- arrays/minimum_swaps_2.py
+++ arrays/minimum_swaps_2.py
@@ -7,30 +7,44 @@ import re
 import sys
 
 # Complete the minimumSwaps function below.
-def minimumSwaps(arr):
-    swaps = 0
-    for i in xrange(len(arr)):
-        if arr[i] != i+1:
-            for j in xrange(i+1, len(arr)):
-                if arr[j] == i+1:
-                    tmp = arr[j]
-                    arr[j] = arr[i]
-                    arr[i] = tmp
-                    swaps += 1
-                    break
+# def minimumSwaps(arr):
+#     swaps = 0
+#     for i in xrange(len(arr)):
+#         if arr[i] != i+1:
+#             for j in xrange(i+1, len(arr)):
+#                 if arr[j] == i+1:
+#                     tmp = arr[j]
+#                     arr[j] = arr[i]
+#                     arr[i] = tmp
+#                     swaps += 1
+#                     break
+#
+#     return swaps
+
+def minimumSwaps(arr) -> int:
+    swaps: int = 0
+    index: int = 0
+    while index < len(arr):
+        if arr[index] != index + 1:
+            next_value: int = arr[index]
+            arr[index], arr[next_value - 1] = arr[next_value - 1], arr[index]
+            swaps += 1
+        else:
+            index += 1
 
     return swaps
+
 
 
 if __name__ == '__main__':
     fptr = open('swaps.out', 'w')
 
-    n = int(raw_input())
+    n = int(input())
 
-    arr = map(int, raw_input().rstrip().split())
+    arr = list(map(int, input().rstrip().split()))
 
     res = minimumSwaps(arr)
-    print res
+
     fptr.write(str(res) + '\n')
 
     fptr.close()
